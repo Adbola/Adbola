45f974f0957482b2cd41c21f13af7220088a1109
Adbola <46797030+Adbola@users.noreply.github.com>
2020-03-26 22:43:04 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+let name = "Ajibade Adebola";
+
+let courses = ["HTML", "CSS", "JAVASCRIPT"]
+
+console.log("My name is: " + name + " my courses are " + courses);
+
+const data = [];
+
+for (let i=1; i<200; i+=2) {
+ 
+      data.push(i);
+ 
+    }
+ 
+ 
+  console.log(data);
