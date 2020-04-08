6ea42d2c48c6b013998d3d7880492f5fea7219c5
Adbola <46797030+Adbola@users.noreply.github.com>
2020-04-08 23:47:16 +0100

    Create interest calculator.js

---
+++ interest calculator.js
@@ -0,0 +1,17 @@
+let data = [
+    {Principal: 2500, time: 1.8},
+    {Principal: 1000, time: 5},
+    {Principal: 3000, time: 3},
+    {Principal: 2000, time: 1}
+];
+
+function interestCalculator(arr) {
+    let Principal = object[Principal]
+    let time = object[time]
+    if (Principal >= 2500 && time > 1 && time < 3  == rate == 3); 
+        else if (Principal >= 2500 && time >= 3  == rate == 3);
+        else if  (Principal >= 2500 && time >= 3  == rate == 3);
+        else rate = 1;
+      }
+      interest = (Principal * rate * time) / 100
+console.log(interestData); 
