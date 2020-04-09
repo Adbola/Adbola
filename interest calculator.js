93d7b2cd0da16410b11e707c53b4640e615130de
Adbola <46797030+Adbola@users.noreply.github.com>
2020-04-10 00:34:37 +0100

    Update interest calculator.js

--- interest calculator.js
+++ interest calculator.js
@@ -4,14 +4,25 @@ let data = [
     {Principal: 3000, time: 3},
     {Principal: 2000, time: 1}
 ];
-
-function interestCalculator(arr) {
-    let Principal = object[Principal]
-    let time = object[time]
+ 
+    
+function interestCalculator(data) {
+    for (let i = 0; i<data.lenght; i++)
+    {
     if (Principal >= 2500 && time > 1 && time < 3  == rate == 3); 
         else if (Principal >= 2500 && time >= 3  == rate == 3);
         else if  (Principal >= 2500 && time >= 3  == rate == 3);
-        else rate = 1;
+        else rate = 1;}
+        interest = (Principal * rate * time) / 100
+
       }
-      interest = (Principal * rate * time) / 100
-console.log(interestData); 
+      
+      const interestData = []
+
+      {
+ 
+        interestData.push(interest);
+       
+      }
+   
+      console.log(interestData);
